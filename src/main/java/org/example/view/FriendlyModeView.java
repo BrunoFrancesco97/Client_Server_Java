521631e18cf1a26df780c1177a801838656fc1ce
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-11-10 22:07:32 +0100

    Fix

--- src/main/java/org/example/view/FriendlyModeView.java
+++ src/main/java/org/example/view/FriendlyModeView.java
@@ -38,7 +38,7 @@ public class FriendlyModeView {
         content.setLayout(new GridLayout((matches.size()),1));
         content.setVisible(true);
         scrollable.setViewportView(content);
-
+        print(matches, sender, name, frame, panel, content, mm);
         panel.add(scrollable);
         JButton reload = new JButton("Reload");
         panel.add(reload);
@@ -57,6 +57,35 @@ public class FriendlyModeView {
         panel.setVisible(true);
     }
 
+    private void print(ArrayList<Match> matches, Sender sender, String name, JFrame frame, JPanel panel, JPanel content, MatchChecker mm){
+        for(Match m : matches){
+            JPanel content2 = new JPanel();
+            content2.setLayout(new GridLayout(5,1));
+            JLabel l1 = new JLabel("Match: "+m.getName());
+            content2.add(l1);
+            JLabel l2 = new JLabel("Host: "+m.getHost());
+            content2.add(l2);
+            JLabel l3 = new JLabel("Size: "+m.getPlayers().size());
+            content2.add(l3);
+            JButton enter = new JButton("Enter");
+            content2.add(enter);
+            enter.addActionListener(e2 -> {
+                Message response = sender.sendAndRead(new Message<>(name, "GET_IN",m.getName()));
+                if(response != null && response.getMessage() != null && response.getEvent().equals("GET_IN") && response.getMessage() instanceof Match){
+                    Match mGet = (Match) response.getMessage();
+                    frame.remove(panel);
+                    frame.add(new RoomView(frame, name, matches, mGet, sender, mm).getPanel());
+                    frame.validate();
+                }else{
+                    JOptionPane.showMessageDialog(frame,"Can't join to this match!","Warning!",JOptionPane.WARNING_MESSAGE);
+                }
+            });
+            JSeparator js = new JSeparator();
+            content2.add(js);
+            content.add(content2);
+            content2.setVisible(true);
+        }
+    }
     public JPanel getPanel(){
         return this.panel;
     }
