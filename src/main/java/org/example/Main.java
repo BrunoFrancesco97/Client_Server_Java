0d751ae497f234c296304b61b2c93b4770e5a318
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-11-10 22:24:03 +0100

    updated todo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+//TODO: GESTIRE PRONTO NON PRONTO SU INTERFACCIA FRIENDLY, SE TUTTI I GIOCATORI NEL MATCH SONO READY FAR PARTIRE TIMER START DI TIPO X SECONDI
+//TODO: IMPLEMENTARE TIMER MASSIMO PARTITA FRIENDLY
+//TODO: IMPLEMENTARE TOURNAMENT MODE
+//TODO: IMPLEMENTARE TEST CASES SU SOCKET E VEDERE CHE RISPONDANO QUANTO CI SI ASPETTI
+
+
 public class Main {
     private static final int PORT = 9001;
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
