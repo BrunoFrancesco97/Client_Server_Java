5174266e04a5a9e6793ced9d740799f308f23103
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-12-13 13:02:43 +0100

    Add files via upload

---
+++ client/src/main/java/org/example/controller/ResultsTournamentController.java
@@ -0,0 +1,35 @@
+package org.example.controller;
+
+import org.example.model.MatchChecker;
+import org.example.model.Message;
+import org.example.model.Rank;
+import org.example.utils.Sender;
+import org.example.view.ModeView;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class ResultsTournamentController{
+    public ResultsTournamentController(JFrame frame, JPanel results, JButton jb, String name, Sender sender, MatchChecker mm, ArrayList<Rank> ranks){
+        jb.addActionListener(e -> {
+            sender.send(new Message(name, "COUNT_TOURNAMENT"));
+            frame.remove(results);
+            frame.add(new ModeView(frame,name,sender, mm).getPanel());
+            frame.validate();
+        });
+        float max = ranks.get(0).points;
+        String nameP = ranks.get(0).name;
+        for(Rank r : ranks){
+            if(r.points > max){
+                nameP = r.name;
+                max = r.points;
+            }
+        }
+        if(nameP.equals(name) && max != 0){
+            JOptionPane.showMessageDialog(frame,"You have won the tournament!","Winner!",JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(frame,"You have lost the tournament","You will win next time :(",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
+
