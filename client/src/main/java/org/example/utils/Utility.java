dc20d8a654738e07eda87c0a61939df4b644896e
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2022-12-13 13:01:48 +0100

    Delete client/src/main/java/org/example directory

--- client/src/main/java/org/example/utils/Utility.java
+++
@@ -1,8 +0,0 @@
-package org.example.utils;
-
-public abstract class Utility {
-    public static int randomIDGenerator(int max){
-        return ((int)Math.floor(Math.random()*(max+1)));
-    }
-
-}
